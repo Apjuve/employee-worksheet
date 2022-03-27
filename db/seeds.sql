9f776256ca21ddafca21ab754a1eb3a3cf68994f
Apjuve <abiponce3@hotmail.com>
2022-03-27 19:28:04 -0400

    add first and last name to seeds sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,3 +16,14 @@ VALUES
 ('Accountant', 125000, 3),
 ('Legal Team Lead', 250000, 4),
 ('Lawyer', 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('John', 'Fromm', 1, NULL),
+('Mike', 'Chang', 2, 1), 
+('kevin', 'Perez', 3, NULL), 
+('Kevin', 'Tupac', 4, 3), 
+('Kunal', 'Singh', 5, NULL), 
+('Malia', 'Obama', 6, 5), 
+('Sarah', 'Lord', 7, NULL), 
+('Tom', 'Ivey', 8, 7);
