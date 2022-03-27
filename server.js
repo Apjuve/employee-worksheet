4f7ad1707cbb114e0fa09b2ee8e29d3d3e5114f5
Apjuve <abiponce3@hotmail.com>
2022-03-27 18:36:01 -0400

    add packages

---
+++ server.js
@@ -0,0 +1,7 @@
+// Imports the inquirer npm package
+const inquirer = require("inquirer");
+// Imports the mysql package
+const mysql = require("mysql2");
+// Imports the connection file.
+const connection = require("./db/connection");
+const queries = require("./db/index.js");
