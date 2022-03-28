9f0baae1f884f8b1da04dc94c7a36331df540795
Apjuve <abiponce3@hotmail.com>
2022-03-27 20:33:00 -0400

    add start prompt function  to aallow users to interact with table

--- db/schema.sql
+++ db/schema.sql
@@ -10,3 +10,24 @@ CREATE TABLE department (
   name VARCHAR(100)
  
 );
+
+-- Table for roles in database.
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(100),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+-- EMPLOYEE ROLE TABLE ----
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(100),
+  last_name VARCHAR(100),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+
+);
--- server.js
+++ server.js
@@ -1,7 +1,60 @@
-// Imports the inquirer npm package
+// Imports inquirer npm package
 const inquirer = require("inquirer");
-// Imports the mysql package
+// Imports mysql package
 const mysql = require("mysql2");
-// Imports the connection file.
+// Imports connection file.
 const connection = require("./db/connection");
 const queries = require("./db/index.js");
+
+
+// Starts the inquireer packagage and allows users to answer prompts to interact with tables.
+function startPrompt() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "what would you like to do?",
+          name: "choice",
+          choices: [
+            "View employees",
+            "View roles",
+            "View departments",
+            "Update employee",
+            "Add employee",
+            "Add role",
+            "Add department",
+          ],
+        },
+      ])
+      .then((value) => {
+        switch (value.choice) {
+          case "View employees":
+            viewAllEmployees();
+            break;
+  
+          case "View roles":
+            viewAllRoles();
+            break;
+  
+          case "View departments":
+            viewAllDepartments();
+            break;
+  
+          case "Update employee":
+            updateEmployee();
+            break;
+  
+          case "Add employee":
+            addEmployee();
+            break;
+  
+          case "Add role":
+            addRole();
+            break;
+  
+          case "Add department":
+            addDepartment();
+            break;
+        }
+      });
+  }
